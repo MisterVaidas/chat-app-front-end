291b8b39d28562ce501f993d036c8bc571291060
Chris Palmer <christopherpalmer283@gmail.com>
2023-08-07 15:07:32 +0100

    stlying updated

--- src/App.js
+++ src/App.js
@@ -5,34 +5,27 @@ import Header from "./Components/Header";
 // import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Chat from "./Components/Chat/MainChat";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
+      <Router>
+        <Header />
+        <Nav>
+          {/* <Link to="/">Home</Link> */}
+          <Link to="About">About</Link>
+          <Link to="Chat">Chat</Link>
+        </Nav>
 
-    <Router>
-          <Header />
-          <Nav>
-            {/* <Link to="/">Home</Link> */}
-            <Link to="About">About</Link>
-            <Link to="Chat">Chat</Link>
-          </Nav>
-
-          <Routes>
-            {/* <Route index element={<Home />} /> */}
-            <Route path="/about" element={<About />} />
-            <Route path="/chat" element={<Chat />} />
-          </Routes>
-      
-        </Router>
-  );
-}
+        <Routes>
+          {/* <Route index element={<Home />} /> */}
+          <Route path="/about" element={<About />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
